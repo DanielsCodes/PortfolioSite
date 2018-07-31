7e92a913979a782e9a2484bf01b27ab0fc735984
Daniel Hoornweg <daniel@avantage.nl>
2018-07-31 16:07:07 +0200

    Finished step 4 of the django tuturial.

--- home/views.py
+++ home/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
-def home(request):
-        return HttpResponse("Hello World!")
+from .models import Question, Choice
+
+
+class HomeView(generic.ListView):
+    template_name = 'home/home.html'
+    context_name_object = 'latest_question_list'
+
+    def get_queryset(self):
+        """"Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'home/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'home/results.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'home/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('home:results', args=(question.id,)))
